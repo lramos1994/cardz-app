2ca88c4f64cf6d834e054a7bffb28cb5283841d2
Lucas <lucas_ramos1994@hotmail.com>
2020-01-23 13:48:28 -0300

    Add deck create route and funcionality

---
+++ pages/decks/create.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <form method="post" @submit.prevent="save">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input type="text" class="input" name="email" v-model="name">
+          </div>
+        </div>
+        <div class="control">
+          <button type="submit" class="button is-dark is-fullwidth">Save</button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+import Notification from '~/components/Notification';
+
+export default {
+  components: {
+    Notification,
+  },
+  data() {
+    return {
+      name: null,
+    }
+  },
+  methods: {
+    async save() {
+      try {
+        await this.$axios.post(`deck`, {
+          name: this.name,
+        }).then(response => (
+          console.log(response)
+        ));
+
+        this.$router.push('/decks')
+      } catch (e) {
+        this.error = e.response.data.message
+      }
+    }
+  }
+}
+</script>
--- pages/decks/index.vue
+++ pages/decks/index.vue
@@ -1,38 +1,50 @@
 <template>
   <section class="section">
     <div class="container">
-      <table class="table">
-        <thead>
-          <tr>
-            <th><abbr title="Position">Name</abbr></th>
-            <th>Options</th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <th><abbr title="Position">Name</abbr></th>
-            <th>Options</th>
-          </tr>
-        </tfoot>
-        <tbody>
-          <tr v-for="deck in decks" v-bind:key="deck.id">
-            <td>{{ deck.name }}</td>
-            <td class="has-text-centered">
-              <nuxt-link :to="'/decks/'+deck.id">
-                <span class="icon has-text-success">
-                  <i class="fas fa-check-square"></i>
-                </span>
-              </nuxt-link>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <div class="columns is-vcentered">
+            <div class="column is-3">
+              <h1 class="title is-1 ">Decks</h1>
+            </div>
+            <div class="column has-text-right">
+              <b-button @click="create">Add New</b-button>
+            </div>
+          </div>
+          <table class="table is-fullwidth">
+            <thead>
+              <tr>
+                <th><abbr title="Position">Name</abbr></th>
+                <th class="has-text-centered">Options</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th><abbr title="Position">Name</abbr></th>
+                <th class="has-text-centered">Options</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              <tr v-for="deck in decks" v-bind:key="deck.id">
+                <td>{{ deck.name }}</td>
+                <td class="has-text-centered">
+                  <nuxt-link :to="'/decks/'+deck.id">
+                    <span class="icon has-text-success">
+                      <i class="fas fa-check-square"></i>
+                    </span>
+                  </nuxt-link>
 
-              <nuxt-link to="/">
-                <span class="icon has-text-success">
-                  <b-icon pack="fas" icon="ban" size="is-small"></b-icon>
-                </span>
-              </nuxt-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                  <nuxt-link to="/">
+                    <span class="icon has-text-success">
+                      <b-icon pack="fas" icon="ban" size="is-small"></b-icon>
+                    </span>
+                  </nuxt-link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -48,6 +60,11 @@ export default {
     this.$axios
       .get('deck')
       .then(response => (this.decks = response.data))
+  },
+  methods: {
+    create() {
+      this.$router.push('/decks/create')
+    }
   }
 }
 </script>
